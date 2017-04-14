d0561f7eec5ebacab805310572e7e5f8c3366372
Alexey Prokhorov <megahertz@l96.ru>
2017-04-14 08:15:28 +0500

    Improve jsdoc

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+/* eslint-disable no-unused-vars */
+
 'use strict';
 
 const electron   = require('electron');
@@ -8,20 +10,42 @@ const file   = new ConfigFile();
 const config = new Config(file);
 
 module.exports = {
+  /**
+   * Returns a value associated with the key.
+   * @param {string} key
+   * @param {*} [defaultValue]
+   * @returns {*}
+   */
   get(key, defaultValue = undefined) {
     return config.get(key, defaultValue);
   },
 
+  /**
+   * Sets a value.
+   * @param {string} key
+   * @param {*} value
+   * @returns {IElectronCfg}
+   */
   set(key, value) {
     config.set(key, value);
     return module.exports;
   },
 
+  /**
+   * Removes values associated with the key.
+   * @param {string} key
+   * @returns {IElectronCfg}
+   */
   delete(key) {
     config.delete(key);
     return module.exports;
   },
 
+  /**
+   * Gets / Sets the root object of the config
+   * @param {Object} [data]
+   * @returns {Object|undefined}
+   */
   all(data) {
     if (data) {
       config.writeData(data);
@@ -31,6 +55,11 @@ module.exports = {
     return config.all();
   },
 
+  /**
+   * Gets / Sets config's file path
+   * @param {string} [filePath]
+   * @returns {string|undefined}
+   */
   file(filePath) {
     if (filePath) {
       file.setPath(filePath);
@@ -40,16 +69,32 @@ module.exports = {
     return file.filePath;
   },
 
+  /**
+   * Attaches a handler on keyName property changes. Changes are observable
+   *   only in the same process.
+   * @param {string} key
+   * @param {Function} handler (newValue, oldValue, key) => void
+   * @returns {IElectronCfg}
+   */
   observe(key, handler) {
     config.observe(key, handler);
     return module.exports;
   },
 
+  /**
+   * Removes all data from config
+   * @returns {IElectronCfg}
+   */
   purge() {
     config.purge();
     return module.exports;
   },
 
+  /**
+   * Gets / Sets a logger (object with error, warn and debug methods)
+   * @param {ILogger} logger
+   * @returns {ILogger|undefined}
+   */
   logger(logger) {
     if (logger) {
       file.setLogger(logger);
@@ -59,7 +104,13 @@ module.exports = {
     return file.logger;
   },
 
-  // eslint-disable-next-line no-unused-vars
+  /**
+   * Creates a new BrowserWindow. electron-cfg spies on its state changes.
+   * @param {Object} [windowOptions]
+   * @param {Object} [settingsOptions]
+   * @returns {Electron.BrowserWindow}
+   * eslint-disable-next-line no-unused-vars
+   */
   createBrowserWindow(windowOptions = {}, settingsOptions = {}) {
     const BrowserWindow = electron.BrowserWindow ||
       electron.remote.BrowserWindow;
